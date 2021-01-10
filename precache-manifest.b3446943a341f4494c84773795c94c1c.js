14f2556f2da8756a88cf07a8e79e58a0fa2ebdae
ieadoboe <65624216+ieadoboe@users.noreply.github.com>
2021-01-10 22:12:41 +0000

    Add files via upload

---
+++ precache-manifest.b3446943a341f4494c84773795c94c1c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8df0e1eab5d8e4d24e9887c882863a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44cfe1b51e778062874c",
+    "url": "/static/css/main.8ea17dd8.chunk.css"
+  },
+  {
+    "revision": "3b727b3a9f6cdc3a8399",
+    "url": "/static/js/2.65c4f492.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.65c4f492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44cfe1b51e778062874c",
+    "url": "/static/js/main.eb9842c9.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "6eaa71ef91326aa671adc48eb7d2486b",
+    "url": "/static/media/bulldozer.6eaa71ef.svg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "c101995fe8c0198f9058f0b21ae1180f",
+    "url": "/static/media/features-split-image-01.c101995f.png"
+  },
+  {
+    "revision": "21c64101da1524d9732cfea67c1a3aaa",
+    "url": "/static/media/features-split-image-02.21c64101.jpg"
+  },
+  {
+    "revision": "7d9c723e63a1288cd3e04adad6db08bf",
+    "url": "/static/media/features-split-image-03.7d9c723e.png"
+  },
+  {
+    "revision": "e3366913e989b317a4410084f79fdc23",
+    "url": "/static/media/logo.e3366913.svg"
+  }
+]);
